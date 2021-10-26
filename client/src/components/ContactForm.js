21dcbfc378e1cf8d48c44178e58aad3ed7545cf3
rubenSinzig <ruben.sinzig@posteo.de>
2021-10-26 10:49:16 +0200

    add label & input for Contact Form

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -5,7 +5,25 @@ const ContactForm = () => {
     <div className="wrapper-form">
       {/* Form */}
       <div className="form-container">
-        <form></form>
+        {/* <form action="https://formspree.io/f/{form_id}" method="post"> */}
+        <form>
+          <label>
+            Name
+            <input type="text" name="name" />
+          </label>
+
+          <label>
+            E-Mail
+            <input type="email" name="email" />
+          </label>
+
+          <label>
+            Tel
+            <input type="tel" name="phone" />
+          </label>
+
+          <input type="submit" value="Send" />
+        </form>
       </div>
 
       {/* Map */}
